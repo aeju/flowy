86aca6a1535350797cdd5c1fb64d03876039bf5c
AJ Choe <aejuchoe@naver.com>
2026-07-06 22:16:55 +0900

    feat: Logic/View/BootStrap 폴더 추가 및 Bootstrapper 스크립트 추가
    * 폴더 구조
    - Logic : 순수 C#
    - View : UI (Unity)
    - Bootstrap : 조립 및 코드 진입점

---
+++ Assets/02. Scripts/Bootstrap/Bootstrapper.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Flowy.Bootstrapper
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        // 역할: 유일한 진입점. Logic 객체 생성 및 주입, tick 구동
+        // 빈 껍데기 -> 기능 추가될 때마다 추가
+
+        // Logic 객체 생성 + 서로 연결(주입)
+        private void Awake()
+        {
+            Debug.Log("Bootstrap 시작");
+        }
+
+        private void Start()
+        {
+            Debug.Log("Start");
+        }
+
+        // 매 프레임 Logic의 Tick 구동
+        private void Update()
+        {
+            Debug.Log("Update");
+        }
+    }
+}
+
