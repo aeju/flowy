4ffd16e953107167dbe198ad9b9ee606bb954cfe
AJ Choe <aejuchoe@naver.com>
2026-08-19 23:50:18 +0900

    feat: MVVM 공정 목록 바인딩 구현 (첫 View/ViewModel 연결)
    - MainViewModel 추가: ObservableCollection으로 공정 목록(W1~W4) 노출
    - MainWindow에 DataContext로 ViewModel 연결
    - XAML ListBox를 Processes에 바인딩해 화면 표시
    - WPF 데이터 바인딩 첫 동작 확인 (하드코딩 데이터, Core 연결은 다음 단계)

--- flowy-wpf/Flowy.Wpf/MainWindow.xaml.cs
+++ flowy-wpf/Flowy.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Flowy.Wpf.ViewModels;    // MainViewModel을 쓰기 위함
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,7 +19,9 @@ namespace Flowy.Wpf
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();    // XAML을 로드해 화면 구성 (WPF 기본)
+            DataContext = new MainViewModel(); // 이 창이 바라볼 ViewModel 지정
+                                               // DataContext = "이 화면의 데이터 공급원은 이거다"라는 선언 (이게 있어야 XAML의 {Binding}이 ViewModel을 찾아감)
         }
     }
 }
---
+++ flowy-wpf/Flowy.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;   // ObservableCollection을 쓰기 위함
+
+namespace Flowy.Wpf.ViewModels
+{
+    public class MainViewModel  // public이어야 XAML에서 접근 가능 (internal이면 바인딩 안 됨)
+    {
+        // ObservableCollection = "변경 시 화면에 자동 알림"이 내장된 목록 (= 이벤트 버스의 WPF 기본형)
+        // 일반 List<string>을 쓰면 항목 추가/삭제가 화면에 자동 반영이 안 됨
+        public ObservableCollection<string> Processes { get; }  // { get; } = 외부에서 읽기만 가능
+
+        public MainViewModel()
+        {
+            Processes = new ObservableCollection<string>
+            {
+                "W1 - 가동",  // 지금은 하드코딩.
+                "W2 - 대기",
+                "W3 - 가동",
+                "W4 - 이상"
+            };
+        }
+    }
+}
