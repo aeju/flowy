fb7a64467e4340248d2fc85ee4e04bd9ec8a9d22
AJ Choe <aejuchoe@naver.com>
2026-09-05 23:28:53 +0900

    feat: 공정 상태 색상 표시 (IValueConverter)
    - StateToColorConverter로 공정 상태(가동/대기/이상/정지)를 색으로 구분
    - 표현 로직을 View 계층(Converter)에 분리해 ViewModel은 상태 데이터만 유지

---
+++ flowy-wpf/Flowy.Wpf/Converters/StateToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;    // CultureInfo(변환 메서드 파라미터)를 쓰기 위함
+using System.Windows.Data;     // IValueConverter를 쓰기 위함
+using System.Windows.Media;    // Brushes(SeaGreen 등 색)를 쓰기 위함
+
+namespace Flowy.Wpf.Converters
+{
+    // 공정 상태(enum) -> 표시 색으로 변환
+    internal class StateToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var state = value?.ToString();
+            return state switch // Brushes: WPF의 칠하는 도구 모음
+            {
+                "가동" => Brushes.SeaGreen,    // Running
+                "대기" => Brushes.Goldenrod,   // Idle
+                "이상" => Brushes.Firebrick,   // Error
+                "정지" => Brushes.Gray,        // Stopped
+                _ => Brushes.Black
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException(); // 역변환 불필요
+    }
+}
